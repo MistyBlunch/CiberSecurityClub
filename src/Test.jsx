f964f82bcc567e21927b3d2e67409d8e594f22e2
MistyBlunch <gracenikole@gmail.com>
2020-04-26 20:45:33 -0500

    webpage

---
+++ src/Test.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function Test() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Test Page
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Test;
