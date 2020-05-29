934355d9a73c52850cf3f3ede63808b7b9de970e
MistyBlunch <gracenikole@hotmail.com>
2020-05-28 21:31:13 -0500

    Structure updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import './App.css';
-import Parallax from "./components/Parallax";
-
+import Parallax from "./components/Parallax/index";
 
 function App() {
   return (
     <div className="App">
       <Parallax />
+      {/* 
+        Aquí se agregan los componentes que irán creando, en 
+        este mismo ordem
+      */}
     </div>
   );
 }
