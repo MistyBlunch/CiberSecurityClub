7a8960f1c8d07209344c9a99ec057e0922e5b09b
MistyBlunch <gracenikole@hotmail.com>
2020-05-31 19:02:26 -0500

    names of components changed

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -4,28 +4,28 @@ import './main.js';
 import logo from './img/Mariquita.svg'
 
 function NavBar(){
-		return(
-		<div className="navbar-fixed" >
-			<nav className="backgroundColor">
-				<div className="nav-wrapper backgroundColor">
-				<div className="margen">
-					<img src={logo} className="left iconSize"/>
-					<a href="#!" className="brand-logo no-abs left">CyberSecurity Club</a>
-					<a href="#" data-target="mobile-demo" className="sidenav-trigger right"><i className="material-icons">menu</i></a>
-					<ul className="right hide-on-med-and-down">
-						<li className="active"><a href="#!">Directives</a></li>
-						<li className="active"><a href="#!">About us</a></li>
-						<li className="active"><a href="#!">Contact</a></li>
-					</ul>
-				</div>
-				</div>
-			</nav>
-			<ul className="sidenav" id="mobile-demo">
-				<li><a href="#">Inicio</a></li>
-				<li><a href="#">Nosotros</a></li>
-				<li><a href="#">Contacto</a></li>
-			</ul>
-		</div>
+	return(
+	<div className="navbar-fixed" >
+		<nav className="backgroundColor">
+			<div className="nav-wrapper backgroundColor">
+			<div className="margen">
+				<img src={logo} className="left iconSize"/>
+				<a href="#!" className="brand-logo no-abs left">CyberSecurity Club</a>
+				<a href="#" data-target="mobile-demo" className="sidenav-trigger right"><i className="material-icons">menu</i></a>
+				<ul className="right hide-on-med-and-down">
+					<li className="active"><a href="#!">Directives</a></li>
+					<li className="active"><a href="#!">About us</a></li>
+					<li className="active"><a href="#!">Contact</a></li>
+				</ul>
+			</div>
+			</div>
+		</nav>
+		<ul className="sidenav" id="mobile-demo">
+			<li><a href="#">Inicio</a></li>
+			<li><a href="#">Nosotros</a></li>
+			<li><a href="#">Contacto</a></li>
+		</ul>
+	</div>
 	);
 };
 
