ce4420b8346b0d8ad6310e7118015f307bbb2255
MistyBlunch <gracenikole@hotmail.com>
2020-05-31 19:48:22 -0500

    Parallax name changed to Slider

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import 'materialize-css/dist/css/materialize.min.css'
 
-import Parallax from "./components/Parallax/index"
+import Parallax from "./components/Slider/index"
 import NavBar from './components/NavBar/index'
 
 function App() {
