61400e8ecbdabcccf7efe23fec68c8432d128d3c
Jhony Angulo <jhony.angulo@utec.edu.pe>
2020-06-06 00:02:35 -0500

    Updated script

--- app.sh
+++ app.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 CMD=docker
-VOL=$PWD:/usr/src/app
+WORKDIR=/usr/src/app
 PORT=8000:3000
-IMAGE=cybersecurity
+IMAGE=cybersecurityclub
+NAME=frontend-csc
+NODE_MODULES_VOL=frontend-csc-node
 
 menu () {
     echo -e "\e[1mDebe poner como parámetro una de las siguientes opciones:\e[0m"
@@ -15,20 +17,29 @@ menu () {
 case $1 in
     build)
         echo -e "\e[1;92mcreando la imagen de \e[1;91;4m"$IMAGE"\e[0m"
+        $CMD volume create $NODE_MODULES_VOL
+        echo -e "\e[1;92mcreando la imagen de \e[1;91;4m"$IMAGE"\e[0m"
         $CMD build -t $IMAGE .
         echo -e "\e[1;92mfin de la creacion de la imagen demo\e[0m"
         echo -e "\e[1;92mInstalacion de las dependencias de "$IMAGE"\e[0m"
-	    $CMD run --rm -it  -v $VOL $IMAGE bash -c "npm install"
+	    $CMD run --rm -it --name $NAME\
+             -v $PWD:$WORKDIR\
+             -v $NODE_MODULES_VOL:$WORKDIR/node_modules\
+             $IMAGE bash -c "npm install"
         echo -e "\e[1;92mFin de la instalacion de las dependencias de demo\e[0m"
         ;;
     bash)
         echo -e "\e[1;92mEjecución de bash en la imagen\e[0m"
-	    $CMD run -p $PORT --rm -it -v $VOL $IMAGE bash
+	    $CMD run -p $PORT --rm -it --name $NAME\
+             -v $PWD:$WORKDIR\
+             -v $NODE_MODULES_VOL:$WORKDIR/node_modules\
+             $IMAGE bash
         echo -e "\e[1;92mFin de la ejecución de bash\e[0m"
         ;;
     run)
         echo -e "\e[1;92mEjecución de la demo de identiface en el puerto externo 3000\e[0m"
-	    $CMD run -p $PORT --rm -it -v $VOL $IMAGE bash -c "npm run start"
+	    $CMD run -p $PORT --rm -it --name $NAME -v $PWD:$WORKDIR \
+             -v $NODE_MODULES_VOL:$WORKDIR/node_modules $IMAGE
         echo -e "\e[1;92mFin de la ejecución\e[0m"
         ;;
     *)
